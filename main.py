16d59be0afd2345276f9c05b3a37ddfa4014364e
huihualiang <hlia597@aucklanduni.ac.nz>
2020-04-29 21:56:29 +1200

    commit

--- main.py
+++ main.py
@@ -1,17 +1,212 @@
-# from __future__ import print_function
+from __future__ import print_function
 import torch
 import torch.optim as optim
 import torchvision
 from torchvision import datasets, transforms
 from torch.optim.lr_scheduler import StepLR
-# from models.conv import Net
-# from models.rnn_conv import ImageRNN
-from models.alexnet import AlexNet
+from models.conv import Net
+from models.rnn_conv import ImageRNN
 import torch.nn.functional as F
 import matplotlib.pyplot as plt
 import numpy as np
 from PIL import Image
 import datetime
 
-model = AlexNet()
-print("done importing")
+# functions to show an image
+def imsave(img):
+    npimg = img.numpy()
+    npimg = (np.transpose(npimg, (1, 2, 0)) * 255).astype(np.uint8)
+    im = Image.fromarray(npimg)
+    im.save("./results/your_file.jpeg")
+
+
+def train_cnn(log_interval, model, device, train_loader, optimizer, epoch):
+    print("train cnn...")
+    model.train()
+    for batch_idx, (data, target) in enumerate(train_loader):
+        print("test...")
+        data, target = data.to(device), target.to(device)
+        # zero the parameter gradients
+        optimizer.zero_grad()
+        # forward + backward + optimize
+        output = model(data)
+        loss = F.nll_loss(output, target)
+        loss.backward(); optimizer.step()
+        if batch_idx % log_interval == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                100. * batch_idx / len(train_loader), loss.item()))
+
+
+def train_rnn(log_interval, model, device, train_loader, optimizer, epoch):
+    model.train()
+    for batch_idx, (data, target) in enumerate(train_loader):
+        data, target = data.to(device), target.to(device)
+        optimizer.zero_grad()
+        # reset hidden states
+        model.hidden = model.init_hidden()
+        data = data.view(-1, 28, 28)
+        outputs = model(data)
+        criterion = torch.nn.CrossEntropyLoss()
+        loss = criterion(outputs, target)
+        loss.backward(); optimizer.step()
+        if batch_idx % log_interval == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                100. * batch_idx / len(train_loader), loss.item()))
+
+
+def test(model, device, test_loader, RNN):
+    model.eval()
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            if RNN:
+                data = torch.squeeze(data)
+
+            output = model(data)
+
+            test_loss += F.nll_loss(output, target, reduction='sum').item()  # sum up batch loss
+            pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+            correct += pred.eq(target.view_as(pred)).sum().item()
+
+    test_loss /= len(test_loader.dataset)
+
+    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+        test_loss, correct, len(test_loader.dataset),
+        100. * correct / len(test_loader.dataset)))
+
+
+def main():
+    begin_time = datetime.datetime.now()
+    epoches = 3
+    gamma = 0.7
+    log_interval = 10
+    torch.manual_seed(1)
+    save_model = True
+
+    #RNN
+    RNN = False
+    N_STEPS = 28
+    N_INPUTS = 28
+    N_NEURONS = 150
+    N_OUTPUTS = 10
+
+    if RNN:
+        epoches = 10
+
+    # Check whether you can use Cuda
+    use_cuda = torch.cuda.is_available()
+    # Use Cuda if you can
+    device = torch.device("cuda" if use_cuda else "cpu")
+
+
+    ######################3   Torchvision    ###########################3
+    # Use data predefined loader
+    # Pre-processing by using the transform.Compose
+    # divide into batches
+    kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
+    train_loader = torch.utils.data.DataLoader(
+        datasets.MNIST('../data', train=True, download=True,
+                       transform=transforms.Compose([
+                           transforms.ToTensor()
+                       ])),
+        batch_size=64, shuffle=True, **kwargs)
+    test_loader = torch.utils.data.DataLoader(
+        datasets.MNIST('../data', train=False, transform=transforms.Compose([
+                           transforms.ToTensor()
+                       ])),
+        batch_size=1000, shuffle=True, **kwargs)
+
+    # # get some random training images
+    # dataiter = iter(train_loader)
+    # images, labels = dataiter.next()
+    # # img = torchvision.utils.make_grid(images)
+    # # imsave(img)
+    #
+    # # #####################    Build your network and run   ############################
+    # if RNN:
+    #     model = ImageRNN(64, N_STEPS, N_INPUTS, N_NEURONS, N_OUTPUTS, device).to(device)
+    # else:
+    #     model = Net().to(device)
+    #
+    # if RNN:
+    #     optimizer = optim.Adadelta(model.parameters(), lr=0.01)
+    # else:
+    #     optimizer = optim.Adam(model.parameters(), lr=0.001)
+    #
+    # scheduler = StepLR(optimizer, step_size=1, gamma=gamma)
+    #
+    # for epoch in range(1, epoches + 1):
+    #     if RNN:
+    #         train_rnn(log_interval, model, device, train_loader, optimizer, epoch)
+    #     else:
+    #         train_cnn(log_interval, model, device, train_loader, optimizer, epoch)
+    #
+    #     test(model, device, test_loader, RNN)
+    #     scheduler.step()
+    #
+    # if save_model:
+    #     if RNN:
+    #         torch.save(model.state_dict(), "./results/mnist_rnn.pt")
+    #     else:
+    #         torch.save(model.state_dict(), "./results/mnist_cnn.pt")
+    # print(datetime.datetime.now() - begin_time)
+    testtrainedmodel(test_loader, device)
+
+
+def testtrainedmodel(test_loader, device):
+    testmodel = Net().to(device)
+    testmodel.load_state_dict(torch.load("./results/mnist_cnn.pt"))
+    testmodel.eval()
+
+    for data, target in test_loader:
+        plt.figure()
+        output = testmodel(data.to(device))
+        preds = torch.max(output.data, 1)
+        # print(preds)
+        images_so_far = 0
+        # print(data.to(device).size()[0])
+        # print("list " + str(len(preds)))
+
+        for j in range(len(preds[0])):
+
+            images_so_far += 1
+
+            # ax.axis('off')
+            plt.subplot(5, 5, images_so_far)
+            # print(type(format(preds[1][j])))
+            plt.title('{}'.format(preds[1][j]))
+            # plt.tight_layout()
+
+            imshow(data[j])
+            if images_so_far == 25:
+
+                plt.show()
+                input("continue? ")
+                images_so_far = 0
+                # count += 1
+                # if count == 10:
+                #     plt.show()
+                # break
+    # plt.show()
+    # plt.show()
+
+
+def imshow(inp, title=None):
+    """Imshow for Tensor."""
+    inp = inp.numpy().transpose((1, 2, 0))
+    mean = np.array([0.485, 0.456, 0.406])
+    std = np.array([0.229, 0.224, 0.225])
+    inp = std * inp + mean
+    inp = np.clip(inp, 0, 1)
+    plt.imshow(inp)
+    if title is not None:
+        plt.title(title)
+    # plt.pause(0.5)  # pause a bit so that plots are updated
+
+
+if __name__ == '__main__':
+    main()
