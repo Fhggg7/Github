de9385ad3209eeb4993f052d9e457ecc9a82fadf
huihualiang <hlia597@aucklanduni.ac.nz>
2020-04-29 20:06:16 +1200

    commit

--- main.py
+++ main.py
@@ -6,10 +6,12 @@ from torchvision import datasets, transforms
 from torch.optim.lr_scheduler import StepLR
 # from models.conv import Net
 # from models.rnn_conv import ImageRNN
+from models.alexnet import AlexNet
 import torch.nn.functional as F
 import matplotlib.pyplot as plt
 import numpy as np
 from PIL import Image
 import datetime
 
+model = AlexNet()
 print("done importing")
