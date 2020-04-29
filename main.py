8b3ef539465991f90d51da9a06c9ef7631fae047
huihualiang <hlia597@aucklanduni.ac.nz>
2020-04-29 18:52:25 +1200

    commit

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
-print("hello")
-print("bye")
+# from __future__ import print_function
+import torch
+import torch.optim as optim
+import torchvision
+from torchvision import datasets, transforms
+from torch.optim.lr_scheduler import StepLR
+# from models.conv import Net
+# from models.rnn_conv import ImageRNN
+import torch.nn.functional as F
+import matplotlib.pyplot as plt
+import numpy as np
+from PIL import Image
+import datetime
